3b41e0ef331fde66bb867b548a737dda4dfb26f7
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-08-08 16:38:20 -0400

    display new api data

--- web/script.js
+++ web/script.js
@@ -2,25 +2,30 @@ const socket = io("http://localhost:8039");
 
 const results = document.querySelector(".socket-data");
 
-socket.on("get-data", (data)=>{
+socket.on("get-data", (response)=>{
+  const data = response[0];
 
-  const incomingData = [];
-
-  incomingData.push(`Date: ${data.date}`);
-  incomingData.push(`Nanoseconds : ${data.nanoseconds}`);
-  incomingData.push(`Ticker : ${data.ticker}`);
-  incomingData.push(`Bid Size : ${data.bidsize}`);
-  incomingData.push(`Bid Price : ${data.bidprice}`);
-  incomingData.push(`Mid Price : ${data.midprice}`);
-  incomingData.push(`Ask Price : ${data.askprice}`);
-  incomingData.push(`Ask Size : ${data.asksize}`);
-  incomingData.push(`Last Price : ${data.lastprice}`);
-  incomingData.push(`Last Size : ${data.lastsize}`);
-  incomingData.push(`Halted : ${data.halted}`);
-  incomingData.push(`After Hours : ${data.afterhours}`);
-  incomingData.push(`Intermarket Seep Order (ISO) : ${data.ISO}`);
-  incomingData.push(`Oddlot : ${data.oddlot}`);
-  incomingData.push(`NMS Rule 611 : ${data.nmsRule}`);
+  console.log(data);
+  const incomingData = 
+    Object.entries(data)
+      .map((entry) => `${entry[0]}: ${entry[1]}`);
   
   results.innerHTML = incomingData.join("<br>");
 });
+
+
+// {
+//   symbol: 'FB',
+//   sector: 'mediaentertainment',
+//   securityType: 'commonstock',
+//   bidPrice: 173,
+//   bidSize: 100,
+//   askPrice: 189.74,
+//   askSize: 100,
+//   lastUpdated: 1565293565360,
+//   lastSalePrice: 189.72,
+//   lastSaleSize: 100,
+//   lastSaleTime: 1565293561209,
+//   volume: 368884,
+//   marketPercent: 0.0332
+// }
