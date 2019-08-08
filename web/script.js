d1ddb218447097a3a4f30ba3a56de00d5fe4aa33
gomezsantiago <santiago.gomez@weareadaptive.com>
2019-08-08 14:44:36 -0400

    display incoming socket data

--- web/script.js
+++ web/script.js
@@ -3,5 +3,24 @@ const socket = io("http://localhost:8039");
 const results = document.querySelector(".socket-data");
 
 socket.on("get-data", (data)=>{
-  results.textContent = `counting seconds : ${data}`;
+
+  const incomingData = [];
+
+  incomingData.push(`Date: ${data.date}`);
+  incomingData.push(`Nanoseconds : ${data.nanoseconds}`);
+  incomingData.push(`Ticker : ${data.ticker}`);
+  incomingData.push(`Bid Size : ${data.bidsize}`);
+  incomingData.push(`Bid Price : ${data.bidprice}`);
+  incomingData.push(`Mid Price : ${data.midprice}`);
+  incomingData.push(`Ask Price : ${data.askprice}`);
+  incomingData.push(`Ask Size : ${data.asksize}`);
+  incomingData.push(`Last Price : ${data.lastprice}`);
+  incomingData.push(`Last Size : ${data.lastsize}`);
+  incomingData.push(`Halted : ${data.halted}`);
+  incomingData.push(`After Hours : ${data.afterhours}`);
+  incomingData.push(`Intermarket Seep Order (ISO) : ${data.ISO}`);
+  incomingData.push(`Oddlot : ${data.oddlot}`);
+  incomingData.push(`NMS Rule 611 : ${data.nmsRule}`);
+  
+  results.innerHTML = incomingData.join("<br>");
 });
